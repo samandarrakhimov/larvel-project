9fac83358a0a1f444e8d6e5e24ae7abb7b2064f4
saman <samandar@gmail.com>
2023-09-20 13:38:50 +0500

    php-project

---
+++ datebase.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "07saman07";
+$database = "php-blog";
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
